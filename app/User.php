d41c42fadb69e83fab3ba971219eb80380fb344c
JJJ <jingfu99@gmail.com>
2017-08-24 15:37:01 +0800

    prototype of everything

---
+++ app/User.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App;
+
+use App\Comment;
+use App\User;
+use App\Post;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    // user has many posts
+    public function posts()
+    {
+        return $this->hasMany('App\Post','author_id');
+    }
+
+    // user has many comments
+    public function comments()
+    {
+        return $this->hasMany('App\Comment','from_user');
+    }
+
+    public function can_post()
+    {
+        $role = $this->role;
+        if($role == 'author' || $role == 'admin')
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public function is_admin()
+    {
+        $role = $this->role;
+        if($role == 'admin')
+        {
+            return true;
+        }
+        return false;
+    }
+}
