6fc254ea13cae35dcea25aa39dccaaf69ad85159
JJJ <jingfu99@gmail.com>
2018-09-05 13:44:06 +0800

    use token to authorize the api request #6

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -74,6 +74,7 @@
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                   {{ csrf_field() }}
+                  <input type="hidden" name="user_token" value="{{ Auth::user()->api_token }}">
                 </form>
               </li>
             </ul>
