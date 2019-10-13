497b091d270baf5b80deac9eaf68ad0e9de563b7
JJJ <jingfu99@gmail.com>
2019-10-13 14:01:15 +0800

    Revert "[feat] Add basic TR page #13"
    This reverts commit d3500d35ce6f5f68d21a923ec519186dd132ba0a.

--- app/TimeRecord.php
+++ app/TimeRecord.php
@@ -3,31 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Carbon\Carbon;
-
 
 class TimeRecord extends Model
 {
-	/**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = [
-        'checkouted_time'
-    ];
-
     // returns the instance of the user who is author of that post
     public function category()
     {
     	return $this->belongsTo('App\TimeCategory','category_id');
-    }
-
-    public function duriation()
-    {
-    	$checkedAt = Carbon::parse($this->checkouted_time);
-    	$createdAt = Carbon::parse($this->created_at);
-
-    	return $createdAt->diff($checkedAt)->format('%h:%i:%s');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,27 +22,24 @@ Route::get('showtag/{tag}', 'TagController@show');
 // check for logged in user
 Route::group(['middleware' => ['auth']], function()
 {
-	//T.I.M.E
-	Route::get('time_record','TimeRecordController@list');
-
-	// show new post form
-	Route::get('new-post','PostController@create');
-	// save new post
-	Route::post('new-post','PostController@store');
-	// edit post form
-	Route::get('edit/{slug}','PostController@edit');
-	// update post
-	Route::post('update','PostController@update');
-	// delete post
-	Route::get('delete/{id}','PostController@destroy');
-	// display user's all posts
-	Route::get('my-all-posts','UserController@user_posts_all');
-	// display user's drafts
-	Route::get('my-drafts','UserController@user_posts_draft');
-	// add comment
-	Route::post('comment/add','CommentController@store');
-	// delete comment
-	Route::post('comment/delete/{id}','CommentController@distroy');
+ // show new post form
+ Route::get('new-post','PostController@create');
+ // save new post
+ Route::post('new-post','PostController@store');
+ // edit post form
+ Route::get('edit/{slug}','PostController@edit');
+ // update post
+ Route::post('update','PostController@update');
+ // delete post
+ Route::get('delete/{id}','PostController@destroy');
+ // display user's all posts
+ Route::get('my-all-posts','UserController@user_posts_all');
+ // display user's drafts
+ Route::get('my-drafts','UserController@user_posts_draft');
+ // add comment
+ Route::post('comment/add','CommentController@store');
+ // delete comment
+ Route::post('comment/delete/{id}','CommentController@distroy');
 });
 //users profile
 Route::get('user/{id}','UserController@profile')->where('id', '[0-9]+');
