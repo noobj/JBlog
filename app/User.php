f4a01216f664852168ccbd59dd960e05d1c5940b
JJJ <jingfu99@gmail.com>
2019-11-03 17:47:27 +0800

    feat(api):add api auth packages #6

--- app/User.php
+++ app/User.php
@@ -7,10 +7,11 @@ use App\User;
 use App\Post;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
